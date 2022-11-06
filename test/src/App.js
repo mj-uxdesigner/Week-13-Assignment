aa772573feb9e18ff8cc3caa1efd701d73cd1949
Mary Jackson <mjackson9712@gmail.com>
2022-11-06 15:35:49 -0700

    Week 13

---
+++ test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/navbar';
+import Login from './components/login';
+
+function App() {
+  return (
+   <div> 
+    <NavBar/>
+    <Login/>
+   </div>
+  );
+}
+
+export default App;
